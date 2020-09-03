6f79fb53e668bc786d23a18bf4b26b611edf77fe
jiessie <947460633@qq.com>
2020-09-03 15:37:38 +0800

    init resp for wechat moments

---
+++ app/src/main/java/com/example/simple/wechatmomentsapplication/WechatActivity.kt
@@ -0,0 +1,12 @@
+package com.example.simple.wechatmomentsapplication
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class WechatActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_wechat)
+    }
+}
